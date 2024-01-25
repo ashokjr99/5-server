987e0b0ae3b23b09da1e1f38c545c05ae5c38544
ashokjr99 <ashoksharmajr@gmail.com>
2024-01-24 18:17:20 -0600

    delete

--- full_crud/crud_server/controllers/crud-controller.js
+++ full_crud/crud_server/controllers/crud-controller.js
@@ -67,4 +67,53 @@ router.post("/create", (req, res) => {
   }
 });
 
+// [PUT] Updating an item from db
+router.put("/update/:id", (req, res) => {
+  try {
+    // Grabbing the index of an item/obj that matches our param id
+    let indexOfItem = db.findIndex((i) => i.id == req.params.id);
+    // console.log("index of item to update:", indexOfItem);
+
+    db[indexOfItem] = {
+      id: req.params.id,
+      name: req.body.name,
+      category: req.body.category,
+      price: req.body.price,
+      emoji: req.body.emoji,
+    };
+
+    res.status(200).json({
+      Updated: db[indexOfItem],
+      Results: db,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      Error: err,
+    });
+  }
+});
+
+// [DELETE] Remove an item from the db
+router.delete("/delete/:id", (req, res) => {
+  try {
+    let indexOfItem = db.findIndex((i) => i.id == req.params.id);
+    db.splice(indexOfItem, 1);
+
+    db.forEach((i, idx) => {
+      i.id = idx + 1;
+    });
+
+    res.status(200).json({
+      Deleted: 1,
+      Results: db,
+    });
+  } catch (err) {
+    // console.log(err)
+    res.status(500).json({
+      Error: err,
+    });
+  }
+});
+
 module.exports = router;
